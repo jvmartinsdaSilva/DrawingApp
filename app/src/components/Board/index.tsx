b8335bd2d19db874cdb62ee385322a2c9cd54056
jvmartinsdaSilva <jvmartins0855@gmail.com>
2023-12-31 19:19:52 -0300

    Adding commiunication with the server

--- app/src/components/Board/index.tsx
+++ app/src/components/Board/index.tsx
@@ -1,81 +1,99 @@
-import { useRef, useEffect, useState } from "react"
-
+import React, { useRef, useEffect, useState } from "react"
 import io from "socket.io-client"
 
 interface drawProps {
     color?: String,
     size?: Number
 }
-// const [socket, setSocket] = useState(null);
 
 
 const Board = ({ color, size }: drawProps) => {
-    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const canvasRef = useRef<HTMLCanvasElement>(null)
+    const [socket, setSocket] = useState<any>()
 
     useEffect(() => {
-        let isDrawing = false;
-        let lastX = 0;
-        let lastY = 0;
+        const newSocket = io("http://localhost:5000")
+        setSocket(newSocket)
+    }, [])
 
-        const startDrawing = (e: { offsetX: number; offsetY: number; }) => {
+    useEffect(() => {
+
+        if(socket){
+            socket.on("canvasImage", (data: any) => {
+            const image = new Image()
+            image.src = data
+            const canvas = canvasRef.current
+            const ctx = canvas?.getContext("2d")
+            image.onload = () => ctx?.drawImage(image, 0 , 0)
+        })
+        }
+
+        let isDrawing = false
+        let lastX = 0
+        let lastY = 0
+
+        const startDrawing = (e: { offsetX: number, offsetY: number }) => {
             isDrawing = true;
             [lastX, lastY] = [e.offsetX, e.offsetY];
         };
 
-        const draw = (e: { offsetX: number; offsetY: number; }) => {
-            if (!isDrawing) return;
+        const draw = (e: { offsetX: number, offsetY: number }) => {
+            if (!isDrawing) return
 
-            const canvas = canvasRef.current;
-            const ctx = canvas.getContext('2d');
+            const canvasImg = canvasRef.current
+            const dataURL  = canvasImg?.toDataURL()
+            const canvas = canvasRef.current
+            const ctx = canvas.getContext('2d')
+
+
             if (ctx) {
                 ctx.beginPath();
-                ctx.moveTo(lastX, lastY);
-                ctx.lineTo(e.offsetX, e.offsetY);
+                ctx.moveTo(lastX, lastY)
+                ctx.lineTo(e.offsetX, e.offsetY)
                 ctx.stroke();
             }
 
-            [lastX, lastY] = [e.offsetX, e.offsetY];
+            [lastX, lastY] = [e.offsetX, e.offsetY]
+            if(socket){
+                socket.emit("canvasImage", dataURL)
+            }
+
         };
 
-        const endDrawing = () => {
-            const canvas = canvasRef.current;
-            const dataURL = canvas.toDataURL(); // Get the data URL of the canvas content
+        const endDrawing = () => isDrawing = false
 
-            isDrawing = false;
-        };
 
-        const canvas: HTMLCanvasElement | null = canvasRef.current;
-        const ctx = canvasRef.current?.getContext('2d');
+        const canvas: HTMLCanvasElement = canvasRef.current
+        const ctx = canvasRef.current?.getContext('2d')
 
         if (ctx) {
             ctx.strokeStyle = color
-            ctx.lineWidth = size;
+            ctx.lineWidth = size
 
-            ctx.lineCap = 'round';
-            ctx.lineJoin = 'round';
-
+            ctx.lineCap = 'round'
+            ctx.lineJoin = 'round'
 
         }
 
-        canvas.addEventListener('mousedown', startDrawing);
-        canvas.addEventListener('mousemove', draw);
-        canvas.addEventListener('mouseup', endDrawing);
-        canvas.addEventListener('mouseout', endDrawing);
+        canvas.addEventListener('mousedown', startDrawing)
+        canvas.addEventListener('mousemove', draw)
+        canvas.addEventListener('mouseup', endDrawing)
+        canvas.addEventListener('mouseout', endDrawing)
 
         return () => {
-            canvas.removeEventListener('mousedown', startDrawing);
-            canvas.removeEventListener('mousemove', draw);
-            canvas.removeEventListener('mouseup', endDrawing);
-            canvas.removeEventListener('mouseout', endDrawing);
-        };
-    }, [color, size]);
+            canvas.removeEventListener('mousedown', startDrawing)
+            canvas.removeEventListener('mousemove', draw)
+            canvas.removeEventListener('mouseup', endDrawing)
+            canvas.removeEventListener('mouseout', endDrawing)
+        }
+    }, [color, size, socket])
 
     return (
         <canvas
             ref={canvasRef}
             width={600}
             height={400}
-            style={{ background: "white", border: "1px solid black" }}
+            style={{ background: "white", border: "2px solid #222" }}
         >
         </canvas>
     )
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const PORT = 5000
 
 const express = require('express');
 const { createServer } = require('node:http');
+const { emit } = require('node:process');
 const { Server } = require('socket.io');
 
 const app = express();
@@ -10,9 +11,14 @@ const io = new Server(server, {
     cors: "http://localhost:5173/"
 });
 
+let dataInfo
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  if(dataInfo) io.emit("canvasImage", dataInfo)
+  socket.on("canvasImage" , data => {
+    dataInfo = data
+    socket.broadcast.emit("canvasImage", data)
+  })
 });
 
 
